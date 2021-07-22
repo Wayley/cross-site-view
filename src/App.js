aedeaff00893782b4afd6abb4e27ac38a01f1a88
wangzheng <zheng.wang@fih-foxconn.com>
2021-07-22 14:12:56 +0800

    modified the layout

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
+import Home from './views/Home';
+import WidgetList from './views/widget/WidgetList';
+import DashboardList from './views/dashboard/DashboardList';
+import DashboardDetail from './views/dashboard/DashboardDetail';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <MainLayout>
-              <div>home</div>
-            </MainLayout>
-          </Route>
-          <Route path="/widget" exact>
-            <div>widget list</div>
-          </Route>
-          <Route path="/dashboard" exact>
-            <div>dashboard</div>
-          </Route>
-          <Route path="/dashboard/:id" exact>
-            <div>dashboard detail</div>
-          </Route>
-          <Route path="*">
-            <div>404</div>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    // <div className="app">
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <MainLayout children={<Home />} />
+        </Route>
+        <Route path="/widget" exact>
+          <MainLayout children={<WidgetList />} />
+        </Route>
+        <Route path="/dashboard" exact>
+          <MainLayout children={<DashboardList />} />
+        </Route>
+        <Route
+          path="/dashboard/:id"
+          exact
+          children={<DashboardDetail />}
+        ></Route>
+        <Route path="/404">
+          <div>404</div>
+        </Route>
+        <Route path="*">
+          <Redirect
+            to={{
+              pathname: '/404',
+            }}
+          />
+        </Route>
+      </Switch>
+    </Router>
+    // </div>
   );
 }
 
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,7 +1,10 @@
+import SideNav from '../components/SideNav';
 export default function MainLayout({ children }) {
   return (
     <div className="global-main-layout">
-      <div className="global-main-layout-aside"></div>
+      <div className="global-main-layout-aside">
+        <SideNav />
+      </div>
       <div className="global-main-layout-container">{children}</div>
     </div>
   );
