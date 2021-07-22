fe786c8c0d2f6fd2b800d5f1fa96f4c2c5d6d0bf
wangzheng <zheng.wang@fih-foxconn.com>
2021-07-22 14:52:31 +0800

    add flex layout

--- src/views/widget/WidgetList.js
+++ src/views/widget/WidgetList.js
@@ -1,3 +1,14 @@
+import './WidgetList.css';
 export default function WidgetList() {
-  return <div className="widget-list-page">widget list</div>;
+  return (
+    <div className="widget-list-page">
+      <div className="global-flex-wrapper">
+        {new Array(14).fill('widget').map((v, k) => (
+          <div className="global-flex-item widget-item" key={k}>
+            widget {k}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
