d141b3e7f6bce6130556740732d72a7e6c6ad601
wangzheng <zheng.wang@fih-foxconn.com>
2021-07-22 10:15:58 +0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div>home</div>
+        </Route>
+        <Route path="/dashboard" exact>
+          <div>dashboard</div>
+        </Route>
+        <Route path="/dashboard/:id" exact>
+          <div>dashboard detail</div>
+        </Route>
+        <Route path="*">
+          <div>404</div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
