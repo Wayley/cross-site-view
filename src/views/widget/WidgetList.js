873b803808bc6f283fd77da2ed45574af24b9a4b
wangzheng <zheng.wang@fih-foxconn.com>
2021-07-23 10:44:11 +0800

    modified styles

--- src/views/widget/WidgetList.js
+++ src/views/widget/WidgetList.js
@@ -1,31 +1,53 @@
 import { Divider } from 'antd';
-
-import Gauges from './gauge';
-import Liquids from './liquid';
 import './WidgetList.css';
+
+import SphericalLiquid from './liquid/SphericalLiquid';
+import BaseGauge from './gauge/BaseGauge';
+import GradientGauge from './gauge/GradientGauge';
+import MeterGauge from './gauge/MeterGauge';
+import CarPanel from './combination/CarPanel';
+
 export default function WidgetList() {
   const widgetStyle = { height: 200 };
-
   return (
     <div className="widget-list-page">
       <div className="gallery-item-box">
-        {Object.keys(Gauges).map((key) => {
-          const Guage = Gauges[key];
-          return (
-            <div className="gallery-item-chart" key={key}>
-              <Guage style={widgetStyle} />
-            </div>
-          );
-        })}
-        <Divider />
-        {Object.keys(Liquids).map((key) => {
-          const Liquid = Liquids[key];
-          return (
-            <div className="gallery-item-chart" key={key}>
-              <Liquid style={widgetStyle} />
-            </div>
-          );
-        })}
+        <div className="gallery-item-chart">
+          <SphericalLiquid style={widgetStyle} />
+        </div>
+        <div className="gallery-item-chart">
+          <BaseGauge style={widgetStyle} />
+        </div>
+        <div className="gallery-item-chart">
+          <GradientGauge style={widgetStyle} />
+        </div>
+        <div className="gallery-item-chart">
+          <MeterGauge style={widgetStyle} />
+        </div>
+      </div>
+
+      <Divider />
+      <div className="gallery-item-box">
+        <div
+          className="gallery-item-chart"
+          style={{
+            gridColumnStart: 1,
+            gridColumnEnd: 3,
+            gridRowStart: 1,
+            gridRowEnd: 3,
+          }}
+        >
+          <CarPanel />
+        </div>
+        <div className="gallery-item-chart">
+          <MeterGauge style={widgetStyle} />
+        </div>
+        <div className="gallery-item-chart">
+          <BaseGauge style={widgetStyle} />
+        </div>
+        <div className="gallery-item-chart">
+          <SphericalLiquid style={widgetStyle} />
+        </div>
       </div>
     </div>
   );
